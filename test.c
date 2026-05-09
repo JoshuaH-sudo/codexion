d5d90601acd67eddc45c459322c6530359ed548d
JoshuaH-sudo <Joshua_Hoban@proton.me>
2026-05-09 17:31:47 +0200

    feat: implement producer-consumer model using condition variables

--- test.c
+++ test.c
@@ -1,39 +1,74 @@
-// Test file to learn threads in C
+// Test file to learn condition variables in C
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-void* foo(void* arg) {
-    printf("Thread1 is running.\n");
+// Shared state
+pthread_mutex_t	mutex;
+pthread_cond_t	cond;
+bool		resource_ready = false;
 
-    // Explicitly terminate thread
-    pthread_exit(NULL);
+// Producer thread: Prepares resource and signals
+void* producer(void* arg) {
+	printf("Producer: Starting work...\n");
+	sleep(2);  // Simulate work
+	printf("Producer: Resource is ready!\n");
 
-    printf("This will not be executed.\n");
-    return NULL;
+	// Lock mutex before modifying shared state
+	pthread_mutex_lock(&mutex);
+	resource_ready = true;
+	printf("Producer: Signaling ALL waiting threads\n");
+	// Broadcast wakes ALL waiting threads (not just one)
+	pthread_cond_broadcast(&cond);
+	pthread_mutex_unlock(&mutex);
+
+	return NULL;
 }
 
-void* myThreadFunc(void* arg) {
-    while(1) {
-        printf("Thread2 is running...\n");
-        sleep(1);
-    }
-    return NULL;
+// Consumer thread: Waits for resource
+void* consumer(void* arg) {
+	int id = *(int *)arg;
+
+	printf("Consumer %d: Waiting for resource...\n", id);
+
+	// Lock mutex before waiting
+	pthread_mutex_lock(&mutex);
+
+	// Wait: releases lock, sleeps, re-acquires lock when signaled
+	while (!resource_ready) {
+		printf("Consumer %d: Sleeping on condition variable\n", id);
+		pthread_cond_wait(&cond, &mutex);
+	}
+
+	printf("Consumer %d: Got resource! Done.\n", id);
+	pthread_mutex_unlock(&mutex);
+
+	return NULL;
 }
 
-int main() {
-    pthread_t thread1, thread2;
-	// pthread_Create(pointer to var, attributes, function to run, argument to function)
-    pthread_create(&thread1, NULL, foo, NULL);
-	pthread_create(&thread2, NULL, myThreadFunc, NULL);
-    sleep(5);
-    //Requesting to cancel the thread after 5 seconds.
-    pthread_cancel(thread2);
-    // Wait for created thread to finish
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+int main(void) {
+	pthread_t prod, cons1, cons2;
+	int id1 = 1, id2 = 2;
 
-	printf("Main thread finished.\n");
-    return 0;
+	// Initialize mutex and condition variable
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&cond, NULL);
+
+	printf("Main: Creating producer and consumers\n");
+	pthread_create(&prod, NULL, producer, NULL);
+	pthread_create(&cons1, NULL, consumer, &id1);
+	pthread_create(&cons2, NULL, consumer, &id2);
+
+	// Wait for all threads
+	pthread_join(prod, NULL);
+	pthread_join(cons1, NULL);
+	pthread_join(cons2, NULL);
+
+	// Cleanup
+	pthread_mutex_destroy(&mutex);
+	pthread_cond_destroy(&cond);
+
+	printf("Main: All done!\n");
+	return 0;
 }
